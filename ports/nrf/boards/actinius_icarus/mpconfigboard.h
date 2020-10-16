d5fd20594f6282ab92498557d47458c7227569cd
Mads Kristiansen <mkr@logship.io>
2020-10-16 23:11:08 +0200

    further alignment and created rak5010 board

--- ports/nrf/boards/actinius_icarus/mpconfigboard.h
+++ ports/nrf/boards/actinius_icarus/mpconfigboard.h
@@ -3,7 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2018 Glenn Ruben Bakke
+ * Copyright (c) 2020 Actinius
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,25 +24,27 @@
  * THE SOFTWARE.
  */
 
-#define PCA10090
+#define ACTINIUS_ICARUS
 
-#define MICROPY_HW_BOARD_NAME       "PCA10090"
+#define MICROPY_HW_BOARD_NAME       "Actinius Icarus"
 #define MICROPY_HW_MCU_NAME         "NRF9160"
-#define MICROPY_PY_SYS_PLATFORM     "nrf9160-DK"
+#define MICROPY_PY_SYS_PLATFORM     "nrf9160"
 
 #define MICROPY_PY_MACHINE_UART     (1)
 #define MICROPY_PY_MACHINE_HW_PWM   (0)
 #define MICROPY_PY_MACHINE_HW_SPI   (1)
-#define MICROPY_PY_MACHINE_TIMER    (0)
+#define MICROPY_PY_MACHINE_TIMER    (1)
 #define MICROPY_PY_MACHINE_RTCOUNTER (1)
 #define MICROPY_PY_MACHINE_I2C      (1)
 #define MICROPY_PY_MACHINE_ADC      (0)
 #define MICROPY_PY_MACHINE_TEMP     (0)
+#define MICROPY_PY_RANDOM_HW_RNG    (0)
 
 #define MICROPY_MBFS                (0)
+#define MICROPY_VFS                 (1)
 
 #define MICROPY_HW_HAS_LED          (1)
-#define MICROPY_HW_HAS_SWITCH       (0)
+#define MICROPY_HW_HAS_SWITCH       (1)
 #define MICROPY_HW_HAS_FLASH        (0)
 #define MICROPY_HW_HAS_SDCARD       (0)
 #define MICROPY_HW_HAS_MMA7660      (0)
@@ -55,36 +57,25 @@
 #define MICROPY_HW_ENABLE_DAC       (0)
 #define MICROPY_HW_ENABLE_CAN       (0)
 
-#define MICROPY_HW_LED_COUNT        (4)
-#define MICROPY_HW_LED_PULLUP       (0)
+#define MICROPY_HW_LED_TRICOLOR     (1)
+#define MICROPY_HW_LED_PULLUP       (1)
 
-#define MICROPY_HW_LED1             (2) // LED1
-#define MICROPY_HW_LED2             (3) // LED2
-#define MICROPY_HW_LED3             (4) // LED3
-#define MICROPY_HW_LED4             (5) // LED4
+#define MICROPY_HW_LED_RED          (10) // LED1 / RED
+#define MICROPY_HW_LED_GREEN        (11) // LED2 / GREEN
+#define MICROPY_HW_LED_BLUE         (12) // LED3 / BLUE
 
 // UART config
-// VCOM0
-#define MICROPY_HW_UART1_RX         (28)
-#define MICROPY_HW_UART1_TX         (29)
-#define MICROPY_HW_UART1_CTS        (26)
-#define MICROPY_HW_UART1_RTS        (27)
+#define MICROPY_HW_UART1_RX         (6)
+#define MICROPY_HW_UART1_TX         (9)
+#define MICROPY_HW_UART1_CTS        (25)
+#define MICROPY_HW_UART1_RTS        (7)
 #define MICROPY_HW_UART1_HWFC       (1)
-
-/*
-// VCOM2
-#define MICROPY_HW_UART1_RX         (0)
-#define MICROPY_HW_UART1_TX         (1)
-#define MICROPY_HW_UART1_CTS        (15)
-#define MICROPY_HW_UART1_RTS        (14)
-#define MICROPY_HW_UART1_HWFC       (1)
-*/
 
 // SPI0 config
 #define MICROPY_HW_SPI0_NAME        "SPI0"
 
-#define MICROPY_HW_SPI0_SCK         (13)
-#define MICROPY_HW_SPI0_MOSI        (11)
-#define MICROPY_HW_SPI0_MISO        (12)
+#define MICROPY_HW_SPI0_SCK         (20)
+#define MICROPY_HW_SPI0_MOSI        (21)
+#define MICROPY_HW_SPI0_MISO        (22)
 
-#define HELP_TEXT_BOARD_LED         "1,2,3,4"
+#define HELP_TEXT_BOARD_LED         "1,2,3"
